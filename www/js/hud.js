938b1f4d64fe86df4c16fc0015a254c8e2bcd7b4
Niki Eskola <neskola@gmail.com>
2013-03-26 18:33:09 +0200

    Refactoring continues
    Added android project and all the www-stuff

---
+++ www/js/hud.js
@@ -0,0 +1,111 @@
+﻿var ScoreObject = me.HUD_Item.extend({
+    init: function (x, y) {
+        this.parent(x, y);
+        this.font = new me.BitmapFont("32x32_font", 32);
+    },
+
+    draw: function (context, x, y) {
+        this.font.draw(context, this.value, this.pos.x + x, this.pos.y + y);
+    }
+});
+
+var PlayScreen = me.ScreenObject.extend({
+    onResetEvent: function () {
+        me.levelDirector.loadLevel("introduction_map");
+
+        me.game.addHUD(0, 430, 640, 60);
+
+        me.game.HUD.addItem("score", new ScoreObject(620, 20));
+
+        me.game.sort();
+
+        me.input.bindKey(me.input.KEY.ESC, "esc", true);
+        me.input.bindMouse(me.input.mouse.LEFT, me.input.KEY.SPACE);
+        me.input.bindTouch(me.input.KEY.SPACE);
+
+        me.audio.playTrack("runtothehills");
+
+    },
+
+    update: function () {
+    },
+
+    onDestroyEvent: function () {
+        me.game.disableHUD();
+
+        me.audio.stopTrack();
+    }
+});
+
+var TitleScreen = me.ScreenObject.extend({
+    init: function() {
+        this.parent(true);
+
+        this.title = null;
+        this.font = null;
+        this.scrollerfont = null;
+        this.scrollertween = null;
+
+        this.scroller = "ARROW KEYS TO MOVE, SPACE TO PLACE A BOMB (NOT FUNCTIONAL YET), ESC BACK TO MENU...";
+        this.scrollerpos = 600;
+        
+    },
+
+    onResetEvent: function () {
+        if (this.title == null) {
+            this.title = me.loader.getImage("title_screen");
+            this.font = new me.BitmapFont("32x32_font", 32);
+            this.font.set("center");
+
+            this.scrollerfont = new me.BitmapFont("32x32_font", 32);
+            this.scrollerfont.set("left");
+
+        }
+
+        this.scrollerpos = 640;
+
+        this.scrollertween = new me.Tween(this).to({
+            scrollerpos: -3200
+        }, 10000).onComplete(this.scrollover.bind(this)).start();        
+
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+        me.input.bindMouse(me.input.mouse.LEFT, me.input.KEY.ENTER);
+        me.input.bindTouch(me.input.KEY.ENTER);
+
+        me.audio.playTrack("toxicity");
+    },
+
+    scrollover: function () {
+        this.scrollerpos = 640;
+        this.scrollertween.to({
+            scrollerpos: -3200
+        }, 10000).onComplete(this.scrollover.bind(this)).start();
+
+    },
+
+
+    update: function () {
+        if (me.input.isKeyPressed('enter')) {
+            me.state.change(me.state.PLAY);
+        }
+    },
+
+    draw: function (context) {
+        context.drawImage(this.title, 0, 0);
+        this.font.draw(context, "MOBLAST", 320, 160);
+        this.font.draw(context, "PRESS ENTER TO PLAY", 320, 280);
+        this.scrollerfont.draw(context, this.scroller, this.scrollerpos, 440);
+    },
+
+    onDestroyEvent: function () {
+        me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindMouse(me.input.mouse.LEFT);
+        me.input.unbindTouch();
+
+        me.audio.stopTrack();
+
+        this.scrollertween.stop();
+    }
+
+});
+
---
+++ www/js/main.js
@@ -0,0 +1,104 @@
+﻿// load these from backend server
+var g_resources = [{
+    name: "TileA4",
+    type: "image",
+    src: "Assets/TileA4.png"
+}, {
+    name: "introduction_map",
+    type: "tmx",
+    src: "Assets/introduction_map.tmx"
+}, {
+    name: "player",
+    type: "image",
+    src: "Assets/player.png"
+}, {
+    name: "explosion",
+    type: "image",
+    src: "Assets/explosion.png"
+}, {
+    name: "spinning_coin_gold",
+    type: "image",
+    src: "Assets/spinning_coin_gold.png"
+}, {
+    name: "32x32_font",
+    type: "image",
+    src: "Assets/32x32_font.png"
+}, {
+    name: "title_screen",
+    type: "image",
+    src: "Assets/titlescreen.png"
+}, {
+    name: "cling",
+    type: "audio",
+    src: "Assets/",
+    channel: 2
+}, {
+    name: "runtothehills",
+    type: "audio",
+    src: "Assets/",
+    channel: 2
+}, {
+    name: "toxicity",
+    type: "audio",
+    src: "Assets/",
+    channel: 2
+}];
+
+var mapObj = new Object();
+
+var jsApp = {
+    onload: function () {
+							
+		if ($(window).width() > 1279 ) {
+			mapObj.blockSize = 64;
+		} else if ($(window).width() > 640) {
+			mapObj.blockSize = 48;
+		} else {
+			mapObj.blockSize = 32;
+		}
+		
+		$(debug).text(mapObj.blockSize);
+	    
+        if (!me.video.init('jsApp', 20 * mapObj.blockSize, 
+        		12 * mapObj.blockSize, false, 1.0)) {
+            alert("Sorry but your browser does not support html 5 canvas.");
+            return;
+        }
+
+        //me.video.scale(me.video.getScreenFrameBuffer(), 1.5); 
+        
+        me.audio.init("mp3,ogg");
+
+        me.loader.onload = this.loaded.bind(this);
+
+        me.loader.preload(g_resources);
+
+        me.state.change(me.state.LOADING);
+    },
+
+    loaded: function () {
+        me.state.set(me.state.MENU, new TitleScreen());
+
+        me.state.set(me.state.PLAY, new PlayScreen());
+
+        me.entityPool.add("mainPlayer", PlayerEntity);
+        //me.entityPool.add("bomb", BombEntity);
+        me.entityPool.add("CoinEntity", CoinEntity);
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
+        me.input.bindKey(me.input.KEY.SPACE, "space");
+        me.input.bindKey(me.input.KEY.ESC, "esc");
+        //me.debug.renderHitBox = true;        
+        //me.input.bindMouse(me.input.mouse.RIGHT, "rightbutton");        
+
+        me.state.change(me.state.MENU);
+
+    }
+};
+
+window.onReady(function () {
+    jsApp.onload();
+});
+
