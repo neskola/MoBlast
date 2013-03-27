6352c26437d9363acce8b5d4055aedc0282975ff
Niki Eskola <neskola@gmail.com>
2013-03-27 17:43:19 +0200

    new map sizes and textures

--- www/js/entities.js
+++ www/js/entities.js
@@ -1,12 +1,18 @@
-﻿var PlayerEntity = me.ObjectEntity.extend({
+﻿var UP    = 1;
+var RIGHT = 2;
+var DOWN  = 3;
+var LEFT  = 4;
+
+var PlayerEntity = me.ObjectEntity.extend({
     
     init: function (x, y, settings) {
         this.parent(x, y, settings);
         this.setVelocity(3, 3);
-        this.updateColRect(4, 24, 4, 24);
+        this.updateColRect(0, 30, 0, 30);
         this.gravity = 0;
     
         this.direction = null;
+        this.nextDirection = null;
 
         this.addAnimation("walk_down", [0, 1, 2]);        
         this.addAnimation("walk_right", [3, 4, 5]);
@@ -19,59 +25,34 @@
 
     update: function () {
 
-        if (me.input.isKeyPressed('insert')) {
-            
-        }
-
         if (me.input.isKeyPressed('left')) {
-            this.direction = 4;
+            this.nextDirection = LEFT;
         } else if (me.input.isKeyPressed('right')) {
-            this.direction = 2;
-        } /*else {
-            this.vel.x = 0;
-        }*/
-
+            this.nextDirection = RIGHT;
+        } 
         if (me.input.isKeyPressed('up')) {
-            this.direction = 1;
+            this.nextDirection = UP;
         } else if (me.input.isKeyPressed('down')) {
-            this.direction = 3;
-        } /*else {
-            this.vel.y = 0;
-        }*/
+            this.nextDirection = DOWN;
+        } 
 
         if (me.input.isKeyPressed('space')) {
             var touchInputs = me.input.touches;
             var mousePos = me.input.mouse.pos;
             if (touchInputs != null && touchInputs[0] != null && touchInputs[1] != null) {
-                console.assert("touch " + touchInputs[0] + "," + touchInputs[1]);
-                me.game.HUD.updateItemValue("score", 2000);
-            } else if (mousePos != null) {
-                var a = this.pos.x;
-                var b = this.pos.y;
-                var c = mousePos.x;
-                var d = mousePos.y;
-                var x = c - a;
-                var y = d - b;
-                var theta = Math.atan2(-y, x);
-                if (theta < 0)
-                    theta += 2 * Math.PI;
-                var absoluteAngle = (theta * 180) / Math.PI;
+                /*console.assert("touch " + touchInputs[0] + "," + touchInputs[1]);
+                me.game.HUD.updateItemValue("score", 2000);*/
+            } else if (mousePos != null &&
+                !this.checkMouseOnPlayer(mousePos.x, mousePos.y)) {
+                var angle = this.calculateAngle(this.pos.x + 16, this.pos.y + 16, mousePos.x, mousePos.y);
+                this.nextDirection = this.checkDirection(angle);
 
-                var angle = Math.round(absoluteAngle);
-                
-                if (angle >= 45 && angle < 135) {
-                    this.direction = 1;
-                } else if (angle >= 135 && angle < 225) {
-                    this.direction = 4;
-                } else if (angle >= 225 && angle < 315) {
-                    this.direction = 3;
-                } else {
-                    this.direction = 2;
-                }
-
-                $('#debug-text').html("mouse x: " + mousePos.x + ", y:" + mousePos.y + "; player x: " + this.pos.x + ", y:" + this.pos.y + ", angle:" + angle + ", direction:" + this.direction);
+                $('#debug-text').html("mouse x: " + mousePos.x + ", y:" + mousePos.y + "; player x: "
+                    + this.pos.x + ", y:" + this.pos.y + ", angle:" + angle + ", direction:"
+                    + this.direction + ", nextDirection: " + this.nextDirection);
             } else {
-                $('#debug-text').html("me x: " + this.x + ", y:" + this.y);           
+                this.nextDirection = this.direction;
+                $('#debug-text').html("set a bomb on x: " + this.x + ", y:" + this.y);           
             }
             
             //me.entityPool.add("bomb", BombEntity);
@@ -80,9 +61,8 @@
         if (me.input.isKeyPressed('esc')) {
             me.state.change(me.state.MENU);
         }
-        this.checkDirection();
-        this.updateMovement();
-
+        this.checkMovement();
+        
         var res = me.game.collide(this);
 
         if (this.vel.x != 0 || this.vel.y != 0) {
@@ -92,29 +72,89 @@
 
         return false;
     },
+    
+    calculateAngle: function (originX, originY, targetX, targetY) {
+        var x = targetX - originX;
+        var y = targetY - originY;
+        var theta = Math.atan2(-y, x);
+        if (theta < 0)
+            theta += 2 * Math.PI;
+        var absoluteAngle = (theta * 180) / Math.PI;
 
-    checkDirection: function () {
-        
-        if (this.direction == 1) {
-            this.vel.y -= this.accel.y * me.timer.tick;
-            this.vel.x = 0;
-            this.setCurrentAnimation("walk_up");
-        } else if (this.direction == 2) {
-            this.doWalk(false);
-            this.vel.y = 0;
-            this.setCurrentAnimation("walk_left");
-        } else if (this.direction == 3) {
-            this.vel.y += this.accel.y * me.timer.tick;
-            this.vel.x = 0;
-            this.setCurrentAnimation("walk_down");
-        } else if (this.direction == 4) {
-            this.doWalk(true);
-            this.vel.y = 0;
-            this.setCurrentAnimation("walk_left");
+        var angle = Math.round(absoluteAngle);
+        return angle;
+    },
+
+    checkMouseOnPlayer: function (mouseX, mouseY) {
+        if ((mouseX >= this.pos.x && mouseX <= this.pos.x +32) &&
+            (mouseY >= this.pos.y && mouseY <= this.pos.y +32)) {
+            return true;
         } else {
-            this.vel.y = 0;
-            this.vel.x = 0;
+            return false;
         }
+    },
+
+    checkDirection: function (angle) {
+        var direction;
+        if (angle >= 45 && angle < 135) {
+            direction = UP;
+        } else if (angle >= 135 && angle < 225) {
+            direction = LEFT;
+        } else if (angle >= 225 && angle < 315) {
+            direction = DOWN;
+        } else {
+            direction = RIGHT;
+        }
+        return direction;
+    },
+
+    checkMovement: function () {
+        if (this.nextDirection != this.direction) {
+            if (this.nextDirection == UP || this.nextDirection == DOWN
+                && (this.pos.x % 32) == 0) {
+                this.direction = this.nextDirection;
+            }
+            if (this.nextDirection == LEFT || this.nextDirection == RIGHT
+                && (this.pos.y % 32) == 0) {
+                this.direction = this.nextDirection;
+            }
+        }
+
+        switch (this.direction) {
+            case UP:
+                this.vel.y -= this.accel.y * me.timer.tick;
+                this.vel.x = 0;
+                this.setCurrentAnimation("walk_up");
+                break;
+            case RIGHT:
+                this.doWalk(false);
+                this.vel.y = 0;
+                this.setCurrentAnimation("walk_left");
+                break;
+            case DOWN:
+                this.vel.y += this.accel.y * me.timer.tick;
+                this.vel.x = 0;
+                this.setCurrentAnimation("walk_down");
+                break;
+            case LEFT:
+                this.doWalk(true);
+                this.vel.y = 0;
+                this.setCurrentAnimation("walk_left");
+                break;
+            default:
+                this.vel.y = 0;
+                this.vel.x = 0;
+        }
+
+        if (this.pos.x % 32 <= 2) {
+            this.pos.x = Math.floor(this.pos.x / 32) * 32;
+        }
+
+        if (this.pos.y % 32 <= 2) {
+            this.pos.y = Math.floor(this.pos.y / 32) * 32;
+        }
+
+        this.updateMovement();
     }
 
 
